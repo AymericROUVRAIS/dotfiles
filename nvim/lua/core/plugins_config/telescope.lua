6874f0b9151a6fa894d6851a46961674f07078de
Aymeric Rouvrais <arouvrai@enssat.fr>
2026-05-02 19:08:15 +0200

    Fused 2 files

--- nvim/lua/core/plugins_config/telescope.lua
+++ nvim/lua/core/plugins_config/telescope.lua
@@ -2,6 +2,19 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    defaults = {
+      file_ignore_patterns = {
+        "node_modules",
+        ".git/",
+        "build/",
+        "dist/",
+      },
+    },
+  },
+  -- fuzzyfinder
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" }
   },
 }
-
