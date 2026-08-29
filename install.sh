3db57f3162446d4a23fb5390ed5ab5279573a84c
AymericRouvrais <arouvrai@enssat.fr>
2026-08-29 15:43:49 +0200

    Download alacritty themes

--- install.sh
+++ install.sh
@@ -100,7 +100,8 @@ cp $PWD/zsh_config/custom.zsh-theme ~/.oh-my-zsh/themes/
 echo
 echo -e "Adding catppuccin theme to ${GREEN}alacritty${NC}"
 
-curl -LO --output-dir ~/.config/alacritty/colorscheme https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+mkdir -p ~/.config/alacritty/colorschemes
+curl -LO --output-dir ~/.config/alacritty/colorschemes https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
 curl -LO --output-dir ~/.config/alacritty/colorschemes https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
 
 
