3bd96acb0cb6b2e220a7b8f9925f82a65e4b6551
AymericRouvrais <arouvrai@enssat.fr>
2026-09-25 12:12:45 +0200

    Adding full telescope config

--- nvim/lua/core/plugins_config/telescope.lua
+++ nvim/lua/core/plugins_config/telescope.lua
@@ -1,18 +1,18 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    -- dependencies = {
-    --   "nvim-lua/plenary.nvim",
-    -- },
-    -- opts = {
-    --   defaults = {
-    --     file_ignore_patterns = {
-    --       "node_modules",
-    --       "%.git/",
-    --       "build/",
-    --       "dist/",
-    --     },
-    --   },
-    -- },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+          "%.git/",
+          "build/",
+          "dist/",
+        },
+      },
+    },
   },
 }
